dd1281e63f31daf6f3c218574ed82a836a7d1a13
sackie08133 <yunwoo.chung@mail.utoronto.ca>
2026-07-05 21:27:51 -0400

    fix bugs on songList

--- src/pages/SongList.jsx
+++ src/pages/SongList.jsx
@@ -4,13 +4,15 @@ import "./SongList.css"
 function SongList() { 
     return (
        <div className = "song-list">
+            <button className="fab">+</button>
             <div className = "song-object">
-                <div id = "song-description">
+                <div id = "song-description-left">
                     <h1>Title</h1>
                     <h2>BPM, Last Edited</h2>
                 </div>
                 
-                <button>Go</button>
+                    
+             
             </div>
        </div> 
     )
