eb0a92cf759b7929ba5d665a534c0e34f864c04a
sackie08133 <yunwoo.chung@mail.utoronto.ca>
2026-07-13 12:04:50 -0400

    Handle

--- src/components/LoginSignup.jsx
+++ src/components/LoginSignup.jsx
@@ -12,11 +12,45 @@ function LoginSignup({onClose}) {
   const [confirmPassword, setConfirmPassword] = useState('')
 
   const handleLogin = async () => {
-    console.log('login attempt', loginUsername, loginPassword)
+    try { // for network, server errors etc
+        const response = await fetch('http://localhost:3001/login', {
+        method: 'POST', // fetch is by default get
+        headers:{'Content-Type': 'application/json'}, // "tells" express that this is JSON
+        body: JSON.stringify({username: loginUsername, password: loginPassword}) // converts objects into strings
+      })
+
+      const data = await response.json() // parse the response's body
+      if (response.ok) {
+        console.log(data.token) // TEMPORARY, DO NOT KNOW WHAT TO DO WITH TOKEN YET
+        onClose()
+      } else {
+        alert("Something went wrong, Please try again later")
+      }
+    } catch (error) {
+      alert("Server/Internet/etc Error. Please try again later")
+    }
+   
+  
+    
   }
 
   const handleSignup = async () => {
-    console.log('signup attempt', signupUsername, signupPassword)
+    try {
+      const response = await fetch('http://localhost:3001/signup', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username: signupUsername, password: signupPassword, confirmPassword: confirmPassword})
+      })
+      const data = await response.json()
+      if (response.ok) {
+        console.log(data.token)
+        onClose()
+      } else {
+        alert("Something went wrong, Please try again later")
+      }
+    } catch (error) {
+      alert("Server / Internet / etc Error. Please try again later")
+    }
   }
 
   return (
